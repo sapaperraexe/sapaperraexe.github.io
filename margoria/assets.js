cb3585404f5131908661b7fd5e33e8cea38aff6a
YON <58143619+sapaperraexe@users.noreply.github.com>
2026-02-02 04:31:17 -0500

    margoria updates

---
+++ margoria/assets.js
@@ -0,0 +1,78 @@
+// Margoria Assets - Archivo modificable con todos los assets
+window.ASSETS = {
+  fonts: [
+    { family: "capo1880", file: "fonts/Capo1880.woff2" },
+    { family: "pfeffermediaeval", file: "fonts/PfefferMediaeval.woff2" },
+    { family: "codexmanesse", file: "fonts/codex_manesse.woff2" },
+    { family: "cissanthemos", file: "fonts/Cissanthemos.woff2" },
+    { family: "hicopus", file: "fonts/HocOpus-Regular.woff2" },
+    { family: "icigist", file: "fonts/IciGist-Regular.woff2" },
+    { family: "indiebusillis", file: "fonts/Indiebusillis.woff2" },
+    { family: "litteraeignotae", file: "fonts/LitteraeIgnotae.woff2" },
+    { family: "malatemporacurrunt", file: "fonts/Malatemporacurrunt-Regular.woff2" },
+    { family: "pfeffersimpelgotisch", file: "fonts/PfefferSimpelgotisch.woff2" },
+    { family: "quariteregnumdei", file: "fonts/QuaeriteRegnumDei.woff2" },
+    { family: "sprangalongobarda", file: "fonts/SprangaLongobarda-Regular.woff2" },
+    { family: "vulnus", file: "fonts/Vulnus-Regular.woff2" },
+    { family: "zifferaveneta", file: "fonts/Zifferaveneta-Regular.woff2" },
+    { family: "andiamoabruciarglilacasa", file: "fonts/Andiamoabruciarglilacasa-Reg.woff2" },
+    { family: "capoecclesia", file: "fonts/CapoEcclesia-Regular.woff2" },
+    { family: "capolinea", file: "fonts/Capolinea-Regular.woff2" }
+  ],
+  backgrounds: [
+    { name: "Horae ad usum Trecensem (1415)", src: "images/background/horae_ad_usum_trecensem_1415-20.png" },
+    { name: "Chansonnier Cordiforme", src: "images/background/chansonnier_cordiforme_de_montchenu.png" },
+    { name: "Liège – Book of Hours", src: "images/background/liège_book_of_hours_ms_w16.png" },
+    { name: "Belgium 1480, MS M.493", src: "images/background/book_of_hours_belgium_1480_ms_m493.jpg" },
+    { name: "Horae ad usum Trecensem II", src: "images/background/horae_ad_usum_trecensem_1415-20_2.jpg" },
+    { name: "Les voeux du paon", src: "images/background/les_voeux_du_paon_1350.jpg" },
+    { name: "Plain parchment", src: "images/background/parchment.jpg" }
+  ],
+  caps: [
+    { id: "cap_a", src: "images/caps/a.png" },
+    { id: "cap_b", src: "images/caps/b.png" },
+    { id: "cap_c", src: "images/caps/c.png" },
+    { id: "cap_d", src: "images/caps/d.png" },
+    { id: "cap_e", src: "images/caps/e.png" },
+    { id: "cap_f_hist", src: "images/caps/f_scriptores_historiae_augustae.png" },
+    { id: "cap_g", src: "images/caps/g.png" },
+    { id: "cap_h", src: "images/caps/h.png" },
+    { id: "cap_i_hist", src: "images/caps/i_scriptores_historiae_augustae.png" },
+    { id: "cap_j", src: "images/caps/j.png" },
+    { id: "cap_k", src: "images/caps/k.png" },
+    { id: "cap_l", src: "images/caps/l.png" },
+    { id: "cap_m", src: "images/caps/m.png" },
+    { id: "cap_n", src: "images/caps/n.png" },
+    { id: "cap_o", src: "images/caps/o.png" },
+    { id: "cap_p_hist", src: "images/caps/p_scriptores_historiae_augustae.png" },
+    { id: "cap_r", src: "images/caps/r.png" },
+    { id: "cap_s", src: "images/caps/s.png" },
+    { id: "cap_t_hist", src: "images/caps/t_scriptores_historiae_augustae.png" },
+    { id: "cap_u", src: "images/caps/u.png" },
+    { id: "cap_w", src: "images/caps/w.png" },
+    { id: "cap_y", src: "images/caps/y.png" },
+    { id: "cap_z", src: "images/caps/z.png" }
+  ],
+  borders: [
+    { id: "border_goatbird", src: "images/border/lutterell_goatbird_sm.png" },
+    { id: "border_rat", src: "images/border/luttrell_rat_eating_fish_sm.png" },
+    { id: "border_liege_2", src: "images/border/liège_book_of_hours_ms_w16_2.png" },
+    { id: "border_liege_3", src: "images/border/liège_book_of_hours_ms_w16_3.png" },
+    { id: "border_liege_5", src: "images/border/liège_book_of_hours_ms_w16_5.png" },
+    { id: "border_liege_6", src: "images/border/liège_book_of_hours_ms_w16_6.png" }
+  ],
+  marginalias: [
+    { id: "marg_rabbit1", src: "images/marginalias/breviary_of_renaud_1302-1303_rabbit1_sm.png" },
+    { id: "marg_snailcat", src: "images/marginalias/paris_bible_1485_snailcat_sm.png" },
+    { id: "marg_snailwoman", src: "images/marginalias/heures_de_jacques_de_rambures_1460_snailwoman.png" },
+    { id: "marg_snailberries", src: "images/marginalias/bruges_book_of_hours_1510-1525_snailberries_sm.png" },
+    { id: "marg_hedgehog", src: "images/marginalias/book_of_hours_france_paris_1420-1425_hedgehog_sm.png" },
+    { id: "marg_birdreader", src: "images/marginalias/french_book_of_hours_14birdreader_sm.png" },
+    { id: "marg_luteskeleman", src: "images/marginalias/froissarts_chronicles_luteskeleman_sm.png" },
+    { id: "marg_amphisbaena", src: "images/marginalias/luttrell.f.50r_amphisbaena_sm.png" },
+    { id: "marg_fishman", src: "images/marginalias/luttrell_fishman_sm.png" },
+    { id: "marg_bat", src: "images/marginalias/luttrell_bat_sm.png" }
+  ]
+};
+
+console.log("✓ Assets cargados desde assets.js:", window.ASSETS);
